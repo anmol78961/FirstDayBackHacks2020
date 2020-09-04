1606fde51006ee441c1a211d19c8ade3d66fc402
Dustin Smith <45208269+NarrowCoconut@users.noreply.github.com>
2020-09-04 17:34:46 -0400

    Added the basics

--- Logic/Tests.py
+++ Logic/Tests.py
@@ -1,1 +1,17 @@
-# tests to make sure logic works
+# tests to make sure logic 
+
+# Testing Words.py:
+from Words import ReadFile, GenerateWordList
+# GenerateWordList and ReadFile test
+# Easy Test
+# result = GenerateWordList(1)
+# for word in result:
+#     print(word)
+# Medium Test
+# result = GenerateWordList(2)
+# for word in result:
+#     print(word)
+# Hard Test
+result = GenerateWordList(3)
+for word in result:
+    print(word)
--- Logic/Words.py
+++ Logic/Words.py
@@ -1,2 +1,31 @@
 # Grabs and loads the words from dictionary3000.txt to use as the basis for
 #   the initial scramble.
+def ReadFile(dfile):
+    file1 = open(dfile)
+    words1 = file1.readlines()
+    wordsOutput = []
+    for word in words1:
+        output = word.strip()
+        wordsOutput.append(output)
+    return wordsOutput
+
+# difficulty is an int 1 - 3 for easy med hard
+def GenerateWordList(difficulty):
+    wordList = ReadFile("dictionary3000.txt")
+    # easy = (1, 4)
+    # medium = (5, 7)
+    # hard = (8, -1)
+    generatedOutput = []
+    if difficulty == 1:
+        for word in wordList:
+            if len(word) in range(1, 4):
+                generatedOutput.append(word)
+    elif difficulty == 2:
+        for word in wordList:
+            if len(word) in range(5, 7):
+                generatedOutput.append(word)
+    elif difficulty == 3:
+        for word in wordList:
+            if len(word) in range(8, 20):
+                generatedOutput.append(word)
+    return generatedOutput
