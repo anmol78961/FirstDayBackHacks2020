40ec2888158deed283688cbb049200031f879ece
Dustin Smith <45208269+NarrowCoconut@users.noreply.github.com>
2020-09-05 13:14:07 -0400

    points mechanic

---
+++ Logic/Point.py
@@ -0,0 +1,15 @@
+class points:
+    _points = 5
+
+    def extra_point(self):
+        self._points += 1
+    
+    def add_point(self):
+        self._points +=2
+    
+    def add_all_points(self):
+        self.extra_point()
+        self.add_point()
+    
+    def get_points(self):
+        return self._points
