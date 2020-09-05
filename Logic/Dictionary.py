5998429e9635ec7eb6e1f1f80ade5f23e9eed3a1
anmol78961 <56681449+anmol78961@users.noreply.github.com>
2020-09-05 08:11:26 +0400

    Update Dictionary.py

--- Logic/Dictionary.py
+++ Logic/Dictionary.py
@@ -1,27 +1,40 @@
 # Used to make api calls to PyDictionary
-# Checks the submitted word, 
-#               returns true or false, 
+# Checks the submitted word,
+#               returns true or false,
 #               displays word and definition
 import PyDictionary
 from PyDictionary import PyDictionary
 import sys, os
 
+
+# def go_(word):
+#     if PyDictionary(word).getMeanings() == {word: False}:
+#         print("word doesn't exist")
+#     else:
+#         print(PyDictionary(word).getMeanings())
+
+
 # The api force calls the print function when it fails, need to disable
 # Disable
-def blockPrint():
+def block_print():
     sys.stdout = open(os.devnull, 'w')
 
+
 # Restore
-def enablePrint():
+def enable_print():
     sys.stdout = sys.__stdout__
+
 
 # calls the PyDictionary api
 # returns the result
-def SpellCheck(word):
-    blockPrint()
+def spell_check(word):
+    block_print()
     check = PyDictionary.meaning(word)
-    enablePrint()
-    if check:
-        return check
+    enable_print()
+    if check == {word: False}:
+        print("Word doesn't exist")
     else:
-        return "Incorrect"
+        print(check)
+
+
+pass
