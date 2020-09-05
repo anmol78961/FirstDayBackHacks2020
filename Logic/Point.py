c3d04fc8b3589bc69a47ed95ac1d6c7004da56af
Dustin Smith <45208269+NarrowCoconut@users.noreply.github.com>
2020-09-05 13:33:55 -0400

    Update Point.py

--- Logic/Point.py
+++ Logic/Point.py
@@ -1,18 +1,15 @@
 class points:
     _points = 5
 
-    def extra_point():
+    def extra_point(self):
         self._points += 1
     
-    def add_point():
+    def add_point(self):
         self._points +=2
     
-    def add_all_points():
+    def add_all_points(self):
         self.extra_point()
         self.add_point()
-       
-    def spend_points():
-        self._points -= 1
     
-    def get_points():
+    def get_points(self):
         return self._points
