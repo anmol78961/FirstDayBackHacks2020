668fe46a955311cadf9d4d8a87bcfec599e83d15
anmol78961 <56681449+anmol78961@users.noreply.github.com>
2020-09-06 10:35:05 +0400

    Update main.py

--- Logic/main.py
+++ Logic/main.py
@@ -1,3 +1,10 @@
-import level1
+import level1 as __level
 
-level1.start()
+__level.start("datyo", ["today", "toady", "toad", "toy", "tad"], 1)
+__level.start("igyleln", ["yelling", "eying", "lying", "glen", "lien"], 2)
+__level.start("irifgd", ["frigid", "rigid", "grid", "gird", "rig"], 3)
+__level.start("teerh", ["there", "three", "ether", "here", "tee"], 4)
+__level.start("voiinl", ["violin", "lion", "loin", "oil", "nil"], 5)
+__level.start("pclbiu", ["public", "picul", "blip", "cub", "lip"], 6)
+__level.start("jnleuj", ["jungle", "lunge", "lung", "lune", "june"], 7)
+__level.start("bcutcka", ["cutback", "buck", "tuba", "tack", "tab"], 8)
